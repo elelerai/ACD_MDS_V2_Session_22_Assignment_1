92152019bfb00faeb93b973c35e19fed70110519
elelerai <32876328+elelerai@users.noreply.github.com>
2018-09-19 23:55:43 +0300

    Add files via upload

---
+++ ACD_MDS_V2_Session_22_Assignment_1.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 23:02:14 2018
+
+@author: Eliud Lelerai
+"""
+
+# Importing the libraries
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from patsy import dmatrices
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from sklearn.cross_validation import cross_val_score
+dta = sm.datasets.fair.load_pandas().data
+dta.columns
+
+# add "affair" column: 1 represents having affairs, 0 represents not
+
+dta['affair'] = (dta.affairs > 0).astype(int)
+y, X = dmatrices('affair ~ rate_marriage + age + yrs_married + children + \
+religious + educ + C(occupation) + C(occupation_husb)',
+dta, return_type="dataframe")
+
+X = X.rename(columns = {'C(occupation)[T.2.0]':'occ_2',
+'C(occupation)[T.3.0]':'occ_3',
+'C(occupation)[T.4.0]':'occ_4',
+'C(occupation)[T.5.0]':'occ_5',
+'C(occupation)[T.6.0]':'occ_6',
+'C(occupation_husb)[T.2.0]':'occ_husb_2',
+'C(occupation_husb)[T.3.0]':'occ_husb_3',
+'C(occupation_husb)[T.4.0]':'occ_husb_4',
+'C(occupation_husb)[T.5.0]':'occ_husb_5',
+'C(occupation_husb)[T.6.0]':'occ_husb_6'})
+
+y = np.ravel(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Compute precision, recall, F-measure and support
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
